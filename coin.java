ed3852fe4e9a92bb48ded326e297de5a47ad8c3c
HASHIMOTO_Takumi <s1290121@u-aizu.ac.jp>
2022-07-11 23:35:27 +0900

    HoT: initial revision

---
+++ coin.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class coin{
+    public static void main(String[] args){
+
+
+	System.out.println("Tossing a coin...");
+	int head = 0;
+	int tail = 0;
+	for(int i = 1;i <= 3; i++){
+	    int coin = new java.util.Random().nextInt(5);
+	    if(coin%2 == 0){
+		System.out.println("Round " + i + ": Heads");
+		head++;
+	    } else if(coin%2 == 1){
+		System.out.println("Round " + i + ": Tails");
+		tail++;
+	    }
+	}
+	System.out.println("Heads: " + head + ", Tails: " + tail);
+    }
+}
